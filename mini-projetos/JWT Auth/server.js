999b804eb1638b8beb5dd5c8aa42e92d63aa8ea2
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-04 16:50:34 -0300

    abstração do serviço de autenticação

--- mini-projetos/JWT Auth/server.js
+++ mini-projetos/JWT Auth/server.js
@@ -17,18 +17,20 @@ const posts = [
     }
 ]
 
-app.get('/posts', (req, res) => {
-    return res.json(posts);
+app.get('/posts', authenticateToken, (req, res) => {
+    return res.json(posts.filter(post => post.username === req.user.name));
 });
 
-app.post('/login', (req, res) => {
-    // For a autenticated user
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.sendStatus(401);
 
-    const username = req.body.username;
-    const user = { name: username };
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken });
-});
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    })
+}
 
 app.listen(3000);
